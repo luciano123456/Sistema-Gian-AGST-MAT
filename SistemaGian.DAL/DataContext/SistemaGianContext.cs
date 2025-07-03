de264f212b9f2fffb27f2934a80ae2c890c5bb3c
Luciano <Luciano_5258@hotmail.com>
2025-07-02 21:35:44 -0300

    .

--- SistemaGian.DAL/DataContext/SistemaGianContext.cs
+++ SistemaGian.DAL/DataContext/SistemaGianContext.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using SistemaGian.Models;
 
 namespace SistemaGian.DAL.DataContext;
 
 public partial class SistemaGianContext : DbContext
 {
-    public SistemaGianContext()
-    {
-    }
+
+    private readonly IConfiguration _configuration;
+
 
     public SistemaGianContext(DbContextOptions<SistemaGianContext> options)
         : base(options)
     {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configuration.GetConnectionString("SistemaDB");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 
     public virtual DbSet<Chofer> Choferes { get; set; }
@@ -60,9 +70,6 @@ public partial class SistemaGianContext : DbContext
 
     public virtual DbSet<ZonasCliente> ZonasClientes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-3MT5F5F; Database=Sistema_Gian; Integrated Security=true; Trusted_Connection=True; Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
