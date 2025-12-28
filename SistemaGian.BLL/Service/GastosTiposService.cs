94ee0f9d2fe9e3bfc544522c3dc3e6ae209d2bd7
Luciano <Luciano_5258@hotmail.com>
2025-12-28 01:36:45 -0300

    Commit

---
+++ SistemaGian.BLL/Service/GastosTiposService.cs
@@ -0,0 +1,44 @@
+﻿using SistemaGian.DAL.Repository;
+using SistemaGian.Models;
+
+namespace SistemaGian.BLL.Service
+{
+    public class GastosTiposService : IGastosTiposService
+    {
+
+        private readonly IGastosTiposRepository<GastosTipo> _contactRepo;
+
+        public GastosTiposService(IGastosTiposRepository<GastosTipo> contactRepo)
+        {
+            _contactRepo = contactRepo;
+        }
+        public async Task<bool> Actualizar(GastosTipo model)
+        {
+            return await _contactRepo.Actualizar(model);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            return await _contactRepo.Eliminar(id);
+        }
+
+        public async Task<bool> Insertar(GastosTipo model)
+        {
+            return await _contactRepo.Insertar(model);
+        }
+
+        public async Task<GastosTipo> Obtener(int id)
+        {
+            return await _contactRepo.Obtener(id);
+        }
+
+
+        public async Task<IQueryable<GastosTipo>> ObtenerTodos()
+        {
+            return await _contactRepo.ObtenerTodos();
+        }
+
+
+
+    }
+}
---
+++ SistemaGian.BLL/Service/IGastosTiposService.cs
@@ -0,0 +1,16 @@
+﻿using SistemaGian.Models;
+
+namespace SistemaGian.BLL.Service
+{
+    public interface IGastosTiposService
+    {
+        Task<bool> Eliminar(int id);
+        Task<bool> Actualizar(GastosTipo model);
+        Task<bool> Insertar(GastosTipo model);
+
+        Task<GastosTipo> Obtener(int id);
+
+        Task<IQueryable<GastosTipo>> ObtenerTodos();
+    }
+
+}
