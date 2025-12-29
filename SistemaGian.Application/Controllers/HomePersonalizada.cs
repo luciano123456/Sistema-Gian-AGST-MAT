026d5fe2a0fbd630de64f0acc8acdb670d696fc9
Luciano <Luciano_5258@hotmail.com>
2025-12-29 20:21:20 -0300

    Desarrollo menu personalizado

---
+++ SistemaGian.Application/Controllers/HomePersonalizada.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaGian.Application.Models;
+using SistemaGian.Application.Models.ViewModels;
+using SistemaGian.BLL.Service;
+using SistemaGian.Models;
+using System.Diagnostics;
+
+namespace SistemaGian.Application.Controllers
+{
+
+    [Authorize]
+
+    public class HomePersonalizadaController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
